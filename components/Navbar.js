6e580d63e98219c2ac5bf393aa894c87a2b4e9c4
Akaash Sinha <grendelfearsbeowulf@gmail.com>
2022-01-08 16:18:59 -0500

    built search component

---
+++ components/Navbar.js
@@ -0,0 +1,24 @@
+import { HomeIcon, PuzzleIcon } from "@heroicons/react/outline";
+import Link from "next/link";
+const Navbar = () => {
+  return (
+    <div className="fixed top-0 left-0 h-screen w-16 flex flex-col bg-red-500 drop-shadow-md justify-evenly items-center">
+      <div className="">
+        <Link href="/" passHref>
+          <a>
+            <HomeIcon className="h-12 mx-auto hover:text-white" />
+          </a>
+        </Link>
+      </div>
+      <div className="">
+        <Link href="/" passHref>
+          <a>
+            <PuzzleIcon className="h-12 hover:text-white" />
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
--- pages/pokemon/[name].js
+++ pages/pokemon/[name].js
@@ -54,14 +54,104 @@ const PokemonPage = ({ pokemon }) => {
   console.log(pokemon);
   return (
     <>
-      <h1>{pokemon.name}</h1>
-      <p>{pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name}</p>
-      <p>{pokemon.pokemon_v2_pokemontypes[1]?.pokemon_v2_type.name}</p>
-      <img
-        className=""
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
-        alt={pokemon.name}
-      />
+      <div
+        className={`m-5 p-8 rounded-2xl text-center drop-shadow-md hover:drop-shadow-xl ${
+          pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name == "electric"
+            ? "bg-yellow-400"
+            : ""
+        } ${
+          pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name == "fire"
+            ? "bg-orange-400"
+            : ""
+        } ${
+          pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name == "grass"
+            ? "bg-lime-500"
+            : ""
+        }
+              ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
+                "normal"
+                  ? "bg-stone-300"
+                  : ""
+              } ${
+          pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name == "water  "
+            ? "bg-blue-400"
+            : ""
+        } ${
+          pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name == "psychic"
+            ? "bg-red-400"
+            : ""
+        } ${
+          pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name == "ice"
+            ? "bg-sky-100"
+            : ""
+        }
+              ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
+                "poison"
+                  ? "bg-purple-400"
+                  : ""
+              }
+              ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
+                "water"
+                  ? "bg-blue-400"
+                  : ""
+              }
+              ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name == "bug"
+                  ? "bg-green-500"
+                  : ""
+              }
+              ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
+                "fairy"
+                  ? "bg-pink-500"
+                  : ""
+              }
+              ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
+                "ground"
+                  ? "bg-lime-200"
+                  : ""
+              }
+              ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
+                "fighting"
+                  ? "bg-red-800"
+                  : ""
+              }
+              ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
+                "rock"
+                  ? "bg-amber-700"
+                  : ""
+              }
+              ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
+                "ghost"
+                  ? "bg-purple-700"
+                  : ""
+              }
+              ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
+                "dragon"
+                  ? "bg-indigo-700"
+                  : ""
+              }
+             `}
+      >
+        <div className="flex justify-center">
+          <img
+            className=""
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+            alt={pokemon.name}
+          />
+        </div>
+        <h1>{pokemon.name}</h1>
+        <p>{pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name}</p>
+        <p>{pokemon.pokemon_v2_pokemontypes[1]?.pokemon_v2_type.name}</p>
+      </div>
     </>
   );
 };
