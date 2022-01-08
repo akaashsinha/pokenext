3c4076392fee7d9f95994013cdaa6d946376632f
Akaash Sinha <grendelfearsbeowulf@gmail.com>
2022-01-07 19:10:23 -0500

    start of design

--- pages/test.js
+++ pages/test.js
@@ -1,4 +1,5 @@
 import { ApolloClient, gql, InMemoryCache } from "@apollo/client";
+import Image from "next/image";
 
 export const getStaticProps = async () => {
   const client = new ApolloClient({
@@ -8,7 +9,7 @@ export const getStaticProps = async () => {
   const data = await client.query({
     query: gql`
       query {
-        pokemon_v2_pokemon(order_by: { id: asc }) {
+        pokemon_v2_pokemon(order_by: { id: asc }, limit: 151) {
           name
           id
           pokemon_v2_pokemontypes {
@@ -33,15 +34,102 @@ const Index = ({ data }) => {
           return (
             <div
               key={pokemon.id}
-              className={`m-5 p-2 ${
+              className={`m-5 p-2 rounded-2xl text-center drop-shadow-md hover:drop-shadow-xl ${
                 pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
                 "electric"
-                  ? "bg-yellow-200"
-                  : "bg-white"
-              }`}
+                  ? "bg-yellow-400"
+                  : ""
+              } ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
+                "fire"
+                  ? "bg-orange-400"
+                  : ""
+              } ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
+                "grass"
+                  ? "bg-lime-500"
+                  : ""
+              }
+              ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
+                "normal"
+                  ? "bg-stone-300"
+                  : ""
+              } ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
+                "water  "
+                  ? "bg-blue-400"
+                  : ""
+              } ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
+                "psychic"
+                  ? "bg-red-400"
+                  : ""
+              } ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name == "ice"
+                  ? "bg-sky-100"
+                  : ""
+              }
+              ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
+                "poison"
+                  ? "bg-purple-400"
+                  : ""
+              }
+              ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
+                "water"
+                  ? "bg-blue-400"
+                  : ""
+              }
+              ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name == "bug"
+                  ? "bg-green-500"
+                  : ""
+              }
+              ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
+                "fairy"
+                  ? "bg-pink-500"
+                  : ""
+              }
+              ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
+                "ground"
+                  ? "bg-lime-200"
+                  : ""
+              }
+              ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
+                "fighting"
+                  ? "bg-red-800"
+                  : ""
+              }
+              ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
+                "rock"
+                  ? "bg-amber-700"
+                  : ""
+              }
+              ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
+                "ghost"
+                  ? "bg-purple-700"
+                  : ""
+              }
+              ${
+                pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name ==
+                "dragon"
+                  ? "bg-indigo-700"
+                  : ""
+              }
+             `}
             >
+              {/* Use brightness-0 for name that pokemon */}
               <img
+                className=""
                 src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+                alt={pokemon.name}
               />
               <p className="capitalize">
                 <a
@@ -51,10 +139,10 @@ const Index = ({ data }) => {
                   {pokemon.name}
                 </a>
               </p>
-              <p className="capitalize">
+              <p className="capitalize text-sm">
                 {pokemon.pokemon_v2_pokemontypes[0].pokemon_v2_type.name}
               </p>
-              <p className="capitalize">
+              <p className="capitalize text-sm">
                 {pokemon.pokemon_v2_pokemontypes[1]?.pokemon_v2_type.name}
               </p>
             </div>
